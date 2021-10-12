ca20de1e9b93eac4b55789d3b7d2f7efff4ac407
Sixpacks123 <aub.heurtault@gmail.com>
2021-10-12 14:41:18 +0200

    Update DAO.test.php
    maj dao test

--- modele/DAO.test.php
+++ modele/DAO.test.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 // Projet TraceGPS
 //jules
@@ -86,92 +86,4 @@ unset($dao);
 ?>
 </body> 
 </html>
-=======
-<?php
-//ceci est un test 
-// Projet TraceGPS
-// fichier : modele/DAO.test1.php
-// Rôle : test de la classe DAO.class.php
-// Dernière mise à jour : xxxxxxxxxxxxxxxxx par xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
-// Le code des tests restant à développer va être réparti entre les membres de l'équipe de développement.
-// Afin de limiter les conflits avec GitHub, il est décidé d'attribuer un fichier de test à chaque développeur.
-// Développeur 1 : fichier DAO.test1.php
-// Développeur 2 : fichier DAO.test2.php
-// Développeur 3 : fichier DAO.test3.php
-// Développeur 4 : fichier DAO.test4.php
-
-// Quelques conseils pour le travail collaboratif :
-// avant d'attaquer un cycle de développement (début de séance, nouvelle méthode, ...), faites un Pull pour récupérer
-// la dernière version du fichier.
-// Après avoir testé et validé une méthode, faites un commit et un push pour transmettre cette version aux autres développeurs.
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Test de la classe DAO</title>
-	<style type="text/css">body {font-family: Arial, Helvetica, sans-serif; font-size: small;}</style>
-</head>
-<body>
-<?php 
-// connexion du serveur web à la base MySQL 
-include_once ('DAO.class.php'); 
-//include_once ('_DAO.mysql.class.php'); 
-$dao = new DAO(); 
- 
-// test de la méthode getNiveauConnexion ---------------------------------------------------------- 
-// modifié par DP le 12/8/2021 
-echo "<h3>Test de getNiveauConnexion : </h3>"; 
-$niveau = $dao->getNiveauConnexion("admin", sha1("mdpadmin")); 
-echo "<p>Niveau de ('admin', 'mdpadmin') : " . $niveau . "</br>";  
-$niveau = $dao->getNiveauConnexion("europa", sha1("mdputilisateur")); 
-echo "<p>Niveau de ('europa', 'mdputilisateur') : " . $niveau . "</br>";
-
-$niveau = $dao->getNiveauConnexion("europa", sha1("123456"));
-echo "<p>Niveau de ('europa', '123456') : " . $niveau . "</br>";
-$niveau = $dao->getNiveauConnexion("toto", sha1("mdputilisateur"));
-echo "<p>Niveau de ('toto', 'mdputilisateur') : " . $niveau . "</br>";
-
-
-// test de la méthode existePseudoUtilisateur -----------------------------------------------------
-// modifié par DP le 12/8/2021
-echo "<h3>Test de existePseudoUtilisateur : </h3>";
-if ($dao->existePseudoUtilisateur("admin")) $existe = "oui"; else $existe = "non";
-echo "<p>Existence de l'utilisateur 'admin' : <b>" . $existe . "</b><br>";
-if ($dao->existePseudoUtilisateur("europa")) $existe = "oui"; else $existe = "non";
-echo "Existence de l'utilisateur 'europa' : <b>" . $existe . "</b></br>";
-if ($dao->existePseudoUtilisateur("toto")) $existe = "oui"; else $existe = "non";
-echo "Existence de l'utilisateur 'toto' : <b>" . $existe . "</b></p>";
-
-// test de la méthode getUnUtilisateur -----------------------------------------------------------
-// modifié par dP le 12/8/2021
-echo "<h3>Test de getUnUtilisateur : </h3>";
-$unUtilisateur = $dao->getUnUtilisateur("admin");
-if ($unUtilisateur) {
-    echo "<p>L'utilisateur admin existe : <br>" . $unUtilisateur->toString() . "</p>";
-}
-else {
-    echo "<p>L'utilisateur admin n'existe pas !</p>";
-}
-$unUtilisateur = $dao->getUnUtilisateur("europa");
-if ($unUtilisateur) {
-    echo "<p>L'utilisateur europa existe : <br>" . $unUtilisateur->toString() . "</p>";
-}
-else {
-    echo "<p>L'utilisateur europa n'existe pas !</p>";
-}
-$unUtilisateur = $dao->getUnUtilisateur("admon");
-if ($unUtilisateur) {
-    echo "<p>L'utilisateur admon existe : <br>" . $unUtilisateur->toString() . "</p>";
-}
-else {
-    echo "<p>L'utilisateur admon n'existe pas !</p>";
-}
-
-// ferme la connexion à MySQL :
-unset($dao);
-?>
-</body> 
-</html>
->>>>>>> 9735d9977fee57694080d5ac9714193b1926dc59
